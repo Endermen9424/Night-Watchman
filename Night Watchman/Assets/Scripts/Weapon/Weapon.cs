5e54e7b8561b333dca237d3d2793997c40659cd6
Endermen9424 <ruzgar.cinar0601@gmail.com>
2024-06-28 14:27:06 +0300

    Weapon monoBehavior
    I created Weapon script

---
+++ Night Watchman/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] protected GameObject particle;
+    [SerializeField] protected GameObject cam;
+
+    protected bool auto = false;
+    protected float cooldown = 0;
+    private float timer;
+
+    private void Start() {
+        timer = cooldown;
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+
+        if (Input.GetMouseButton(0)) {
+            shoot();
+        }
+    }
+
+    public void shoot() {
+        if (Input.GetMouseButtonDown(0) && Time.timeScale >= 1 || auto) {
+            if (timer > cooldown) {
+                OnShoot();
+                timer = 0;
+            }
+        }
+    }
+
+    protected virtual void OnShoot() {}
+}
