8ae0a5710fddce072f3f8d5ff3f038b26a044715
Endermen9424 <ruzgar.cinar0601@gmail.com>
2024-07-01 14:20:34 +0300

    Update WalkRobot.cs

--- Night Watchman/Assets/Scripts/Enemy/WalkRobot.cs
+++ Night Watchman/Assets/Scripts/Enemy/WalkRobot.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class WalkRobot : Enemy
 {
     protected override void Walk() {
-        rb.velocity = new Vector3(speed * Time.deltaTime, 0, 0);
+        rb.velocity = new Vector3(-speed, 0, 0);
     }
 }
