5a366acfcd7ba5e1ad4429118ea0541776eab829
Endermen9424 <ruzgar.cinar0601@gmail.com>
2024-07-30 12:55:38 +0300

    .I upgrade

--- Night Watchman/Assets/Scripts/GameManagerScripts/DeahtMenuScript.cs
+++ Night Watchman/Assets/Scripts/GameManagerScripts/DeahtMenuScript.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class DeahtMenuScript : MonoBehaviour
 {
     [SerializeField] GameObject DeahtMenu;
     [SerializeField] GameObject Battle_point_GameArea;
+
+    [SerializeField] TMP_Text Battle_point_Area;
     [SerializeField] GameObject Health_Bar;
 
     public void Deaht() {
@@ -14,6 +17,9 @@ public class DeahtMenuScript : MonoBehaviour
         DeahtMenu.SetActive(true);
         Battle_point_GameArea.SetActive(false);
         Health_Bar.SetActive(false);
+        if (PlayerPrefs.HasKey("Battle_Point")) {
+            Battle_point_Area.text = PlayerPrefs.GetInt("Battle_Point").ToString() + "bp";
+        }
     }
 
     public void Restart(){
